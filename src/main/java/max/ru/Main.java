af73ea1921a1432a2a06f3e1cde4bdd4cfa3bc78
MaksFil <beardofthebear00@gmail.com>
2023-11-10 00:27:07 +0300

    Добавил 2 тестовые строчки вывода в консоль.

--- src/main/java/max/ru/Main.java
+++ src/main/java/max/ru/Main.java
@@ -3,5 +3,7 @@ package max.ru;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello this is Max");
+        System.out.println(1+1*3);
     }
 }
