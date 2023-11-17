c679872d9c7ea4136a6ceee08414f5fbb63e5a6a
MaksFil <beardofthebear00@gmail.com>
2023-11-17 20:55:21 +0300

    Добавил 2 тестовые строчки вывода в консоль.

--- src/main/java/max/ru/Main.java
+++ src/main/java/max/ru/Main.java
@@ -1,10 +1,57 @@
 package max.ru;
 
-public class Main {
+ public class Main {
+     public static void main(String[] args) {
+         String informationPets[] = new String[5];
+         informationPets [0] = "Имя:Тайсон, Возраст:1 год Прививка:да Дата:3.08.2020г";
+         informationPets [1] = "Имя:Барон, Возраст:1.5 года Прививка:нет Дата: ";
+         informationPets [2] ="Имя:Туман, Возраст:2 года Прививка:да Дата:6.08.2022г";
+         informationPets [3] ="Имя:Мура, Возраст:5 лет Прививка:да Дата:12.9.2019г";
+         informationPets [4] ="Имя:Гаечка, Возраст:6 месяцев Прививка:да Дата:11.11.2023г";
 
-    public static void main(String[] args) {
+            for (int i = 0; i < informationPets.length; i ++) {
+                System.out.println(informationPets[i]); // вызов массива
+            }
+     }
+ }
 
-        System.out.println(
+     /*public static void main(String[] args) {
+        String namePet1 = "Тузик";
+        float agePet1 = 1.5f;
+        boolean pet1 = true;
+        System.out.printf ("Name: %s Age: %.1f Graft: %s \n", namePet1, agePet1, pet1);
+
+        String namePet2 = "Бобик";
+        float agePet2 = 2f;
+        boolean pet2 = false;
+        System.out.printf ("Name: %s Age: %.1f Graft: %s \n", namePet2, agePet2, pet2);
+
+        String namePet3 = "Шарик";
+        float agePet3 = 3f;
+        boolean pet3 = false;
+        System.out.printf ("Name: %s Age: %.1f Graft: %s \n", namePet3, agePet3, pet3);
+
+        String namePet4 = "Мурка";
+        float agePet4 = 2.5f;
+        boolean pet4 = true;
+        System.out.printf ("Name: %s Age: %.1f Graft: %s \n", namePet4, agePet4, pet4);
+
+        String namePet5 = "Борис";
+        float agePet5 = 1f;
+        boolean pet5 = true;
+        System.out.printf ("Name: %s Age: %.1f Graft: %s \n", namePet5, agePet5, pet5); //вызов через функцию
+    }
+*/
+   /* public class Pets {
+        public static void main (String[] args){
+            String informationPets [] = new String[5];
+            pet1[0] = "Имя:Тайсон, Возраст:1 год Прививка:да Дата:3.08.2020г";
+            pet2[1] = "Имя:Барон, Возраст:1.5 года Прививка:нет Дата: ";
+             String petsInformation = GiveMePet [0];
+             System.out.println(GiveMePet);
+        }*/
+
+       /* System.out.println(
                 culculate(1, 4)
         );
         System.out.println(
@@ -13,15 +60,17 @@ public class Main {
         System.out.println(
                 removeAllSpaces("Привет, меня зовут Паша и тут будет куча пробелов        ")
         );
+        */
 
-        Game darkSouls = new Game(999, 20, "Dark Souls", "Hardcore", "Ахуенная ебанутая задротсткая игра!");
+
+       /* Game darkSouls = new Game(999, 20, "Dark Souls", "Hardcore", "Ахуенная ебанутая задротсткая игра!");
         darkSouls.getPrice();
 
         System.out.println(
                 darkSouls.toString()
         );
-    }
-
+    } */
+/*
     public static int culculate(int first, int second){
         int result = first + second;
         return result;
@@ -29,9 +78,10 @@ public class Main {
 
     public static String removeAllSpaces(String input){
         return input.replaceAll(" ", "");
-    }
+    } */
 
     /*
+
     Задача на следующий урок
 
     ОСНОВНАЯ ЗАДАЧА
@@ -45,7 +95,10 @@ public class Main {
     И положи их в массив
      */
 
-}
+
+
+
+
 
 
 
