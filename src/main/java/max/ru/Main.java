b5cb22c9955099b65a51eee5347a13fdd43ccecc
PavelEvstafyev <epaulboston@gmail.com>
2023-11-29 00:57:56 +0300

    Задача №3

--- src/main/java/max/ru/Main.java
+++ src/main/java/max/ru/Main.java
@@ -1,25 +1,11 @@
 package max.ru;
 
 public class Main {
-    public static Podcast podcastList[] = new Podcast[3];
 
     public static void main(String[] args) {
-        podcastList[0] = new Podcast("English podcast", " gfgfhhjg");
-        podcastList[1] = new Podcast("Game podcast", " 1238585767");
-        podcastList[2] = new Podcast("News podcast", " zxcvcbvnv");
-        String result = getLinkByName("News podcast", podcastList);
-        System.out.println(result);
+
     }
 
-    public static String getLinkByName(String name, Podcast[] podcastList) {
-        for (Podcast hgjhgj : podcastList
-        ) {
-            if (name == hgjhgj.name) {
-                return hgjhgj.link;
-            }
-        }
-        return "Нет такой ифнормации";
-    }
 }
 
 
